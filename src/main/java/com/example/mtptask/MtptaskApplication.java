f04232b11d68c1bf7028ac35973bbf783735897c
Daniel Filipczak <daniel.filipczak97@gmail.com>
2024-07-28 15:58:52 +0200

    init

---
+++ src/main/java/com/example/mtptask/MtptaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.mtptask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtptaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtptaskApplication.class, args);
+	}
+
+}
